c082f43549b3aada25025a46d022964344369fdc
Avanshu Diwane <avanshudiwane0@gmail.com>
2026-03-15 22:13:10 +0530

    fix: first login wake up issue

---
+++ src/main/java/com/avanshu/smartexpensetracker/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.avanshu.smartexpensetracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Server is Up";
+    }
+}
